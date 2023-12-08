a9f76892b6630a3a01ef772e217f6f728fbf3497
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-12-08 14:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function minDate(dates) {
   //write you code here
+	 if (!dates || dates.length === 0) {
+    return null;
+  }
+
+  // Clone the array to avoid modifying the input array
+  const clonedDates = [...dates];
+
+  // Parse the first date as the initial minimum
+  let minDateValue = new Date(clonedDates[0]);
+
+  // Loop through the remaining dates and update the minimum if needed
+  for (let i = 1; i < clonedDates.length; i++) {
+    const currentDate = new Date(clonedDates[i]);
+
+    if (currentDate < minDateValue) {
+      minDateValue = currentDate;
+    }
+  }
+
+  // Format the minimum date as "YYYY/MM/DD"
+  const minDateFormatted = `${minDateValue.getFullYear()}/${padZeroes(
+    minDateValue.getMonth() + 1
+  )}/${padZeroes(minDateValue.getDate())}`;
+
+  return minDateFormatted;
+}
+
+// Helper function to pad zeroes for single-digit months and days
+function padZeroes(number) {
+  return number.toString().padStart(2, '0');
 }
 
 // Do not change the code
